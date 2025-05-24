dfae6325fef542f6bacf3b43c6dfc6fa4c1f9a47
kieutung52 <kieuthanhtung0502@gmail.com>
2025-05-24 21:13:04 +0700

    Add initial Spring Boot back end

---
+++ backend/character-manage/src/main/java/com/nos/character_manage/CharacterManageApplication.java
@@ -0,0 +1,13 @@
+package com.nos.character_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterManageApplication.class, args);
+	}
+
+}
