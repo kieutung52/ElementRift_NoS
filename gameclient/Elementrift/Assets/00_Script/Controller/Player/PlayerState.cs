5bd203cba1cae4bda5d729970d9e63faca1cbb8b
kieutung52 <kieuthanhtung0502@gmail.com>
2025-05-25 22:06:25 +0700

    Create map with terrian and script move Player

---
+++ gameclient/Elementrift/Assets/00_Script/Controller/Player/PlayerState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Idle,
+    Walking,
+    Jumping,
+    Attack,
+    Falling,
+    Climbing,
+    ClimbingToTop,
+    Dead
+}
